1a70f28877e266aa4855e0c6f72885c88f070ebb
Joe Ulfers <josiah.ulfers@nltgis.com>
2020-10-16 09:42:33 -0400

    #11 add temporary auth0 fingerprint thumbprint

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,7 @@ resource "aws_iam_openid_connect_provider" "default" {
 
   thumbprint_list = [
     "9e99a48a9960b14926bb7f3b02e22da2b0ab7280",
+    "b3dd7606d2b5a8b4a13771dbecc9ee1cecafa38a"
   ]
 }
 
